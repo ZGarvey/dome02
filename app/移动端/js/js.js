0732abb28d600dc4120bdf385d6abe3c761fc762
garvey <garvey@garveydeMacBook-Air.local>
2016-12-19 15:49:48 +0800

    Initial commit

---
+++ app/移动端/js/js.js
@@ -0,0 +1,20 @@
+
+
+/**
+ * Created by Administrator on 2016/11/12.
+ */
+$(function () {
+    
+    var mySwiper = new Swiper ('.swiper-container', {
+
+            loop: true,
+
+            // 如果需要分页器
+            pagination: '.swiper-pagination',
+            effect : 'coverflow',
+            autoplay: 2000,
+            autoplayDisableOnInteraction : false,
+
+        })
+     
+})
